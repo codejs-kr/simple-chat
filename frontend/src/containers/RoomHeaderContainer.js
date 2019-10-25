c632e31eba3ee0a13298104622ed4139f675777f
dodortus <dodortus@gmail.com>
2019-10-26 00:30:42 +0900

    Add RoomHeaderContainer

---
+++ frontend/src/containers/RoomHeaderContainer.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { HeaderTemplate } from 'components';
+
+class RoomHeaderContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      openedUserList: false,
+    };
+  }
+
+  render() {
+    const { users, roomName } = this.props;
+    const userCount = users.length;
+
+    return (
+      <HeaderTemplate>
+        <div className="h-left">
+          <button type="button" id="btn-leave">
+            <i className="material-icons">chevron_left</i>
+          </button>
+        </div>
+        <div className="h-center">{roomName}</div>
+        <div className="h-right">
+          <button type="button" id="btn-people">
+            <span className="count">{userCount}</span>
+            <i className="material-icons">people</i>
+          </button>
+        </div>
+      </HeaderTemplate>
+    );
+  }
+}
+
+export default connect(
+  ({ chat }) => ({
+    users: chat.users,
+    roomName: chat.roomName,
+  }),
+  () => {}
+)(RoomHeaderContainer);
