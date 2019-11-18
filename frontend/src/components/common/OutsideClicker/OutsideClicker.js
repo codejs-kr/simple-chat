2ec6a3886ca029c63c1a9fb4687e799e7538013e
dodortus <dodortus@gmail.com>
2019-11-18 23:06:15 +0900

    Add outside clicker

---
+++ frontend/src/components/common/OutsideClicker/OutsideClicker.js
@@ -0,0 +1,49 @@
+import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+const useOutsideClick = (ref, onClick) => {
+  /**
+   * Alert if clicked on outside of element
+   */
+  function handleClickOutside(event) {
+    if (ref.current && !ref.current.contains(event.target)) {
+      onClick && onClick();
+    }
+  }
+
+  useEffect(() => {
+    // Bind the event listener
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  });
+};
+
+/**
+ * Component that alerts if you click outside of it
+ */
+const OutsideClicker = ({ children, onClick }) => {
+  const wrapperRef = useRef(null);
+  useOutsideClick(wrapperRef, onClick);
+
+  return (
+    <div className="outside-clicker" style={{ display: 'inline-block' }} ref={wrapperRef}>
+      {children}
+    </div>
+  );
+};
+
+OutsideClicker.propTypes = {
+  children: PropTypes.element.isRequired,
+  onClick: PropTypes.func,
+};
+
+OutsideClicker.defaultProps = {
+  onClick: () => {
+    console.log('onClick');
+  },
+};
+
+export default OutsideClicker;
