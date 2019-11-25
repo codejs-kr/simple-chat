f69c8928229f8025bb7e4737a86d64fbad190daf
dodortus <dodortus@gmail.com>
2019-11-25 22:17:49 +0900

    Update socket disconnect handler

--- backend/controllers/socket.js
+++ backend/controllers/socket.js
@@ -17,69 +17,115 @@ module.exports = (http) => {
   const io = require('socket.io')(http);
   let attendees = {}; // 서버에 연결된 사용자, 소켓정보를 담아 관리합니다.
 
-  io.on('connection', (socket) => {
-    console.log('io connection', socket.id);
-    let instanceRoomId = null;
+  /**
+   * 참여자 핸들링
+   * @param {*} userInfo
+   * @param {*} roomId
+   * @param {*} socket
+   */
+  function onJoin(userInfo, roomId, socket) {
+    socket.join(roomId); // 소켓을 특정 room에 binding합니다.
 
-    // 룸접속
-    socket.on('join', (roomId, userInfo) => {
-      instanceRoomId = roomId;
-      socket.join(roomId); // 소켓을 특정 room에 binding합니다.
+    // 유저 목록
+    attendees[userInfo.id] = {
+      ...userInfo,
+      socketId: socket.id,
+    };
 
-      // 유저 목록
-      attendees[userInfo.id] = {
-        ...userInfo,
-        socketId: socket.id,
-      };
+    // 참여자 목록을 함께 내려주기 위해서 참여 당사자에게도 전달한다.
+    io.sockets.in(roomId).emit('join', {
+      userInfo,
+      attendees: attendees,
+    });
+  }
 
-      // 참여자 목록을 함께 내려주기 위해서 참여 당사자에게도 전달한다.
-      io.sockets.in(roomId).emit('join', {
-        userInfo,
-        attendees: attendees,
-      });
-
-      console.log('join', instanceRoomId, Object.keys(attendees).length);
-      // console.log('ROOM LIST', io.sockets.adapter.rooms);
+  /**
+   * 이탈자 핸들링
+   * @param {*} userInfo
+   * @param {*} roomId
+   * @param {*} socket
+   */
+  function onLeave(userInfo, roomId, socket) {
+    socket.leave(roomId);
+    socket.broadcast.to(roomId).emit('leave', {
+      userInfo,
     });
 
-    // 룸퇴장
-    socket.on('leave', (roomId, userInfo) => {
-      socket.leave(roomId);
-      socket.broadcast.to(roomId).emit('leave', {
-        userInfo,
-      });
+    delete attendees[userInfo.id];
+  }
 
-      delete attendees[userInfo.id];
-      instanceRoomId = null;
-
-      console.log('leave', roomId, Object.keys(attendees).length);
-    });
-
-    // 메시징 (userMessage, typing, webrtc signaling, etc)
-    // Ojbect의 key값으로 메시지 type을 구분하여 중복형태의 메시지 전송 리스너 생성을 방지한다.
-    socket.on('message', (data) => {
-      console.log('message', data, instanceRoomId);
-
-      if (!instanceRoomId) {
-        return false;
+  /**
+   * 메시지 핸들링
+   * @param {*} data
+   * @param {*} roomId
+   * @param {*} socket
+   */
+  function onMessage(data, roomId, socket) {
+    // 룸 전체전송
+    if (data.to === 'all') {
+      socket.broadcast.to(roomId).emit('message', data); // 자신 제외 룸안의 유저
+    } else {
+      // 귓속말
+      const targetSocketId = attendees[data.to].socketId;
+      if (targetSocketId) {
+        //io.to(targetSocketId).emit('message', data);
+        io.sockets.connected[targetSocketId].emit('message', data);
       }
+    }
+  }
 
-      // 룸 전체전송
-      if (data.to === 'all') {
-        socket.broadcast.to(instanceRoomId).emit('message', data); // 자신 제외 룸안의 유저
-      } else {
-        // 귓속말
-        const targetSocketId = attendees[data.to].socketId;
-        if (targetSocketId) {
-          //io.to(targetSocketId).emit('message', data);
-          io.sockets.connected[targetSocketId].emit('message', data);
+  /**
+   * 소켓 커넥션 인스턴스 이벤트 바인딩
+   */
+  function bindSocketConnectionEvent() {
+    io.on('connection', (socket) => {
+      console.log('io connection', socket.id);
+      let instanceRoomId = null;
+
+      // 룸접속
+      socket.on('join', (roomId, userInfo) => {
+        instanceRoomId = roomId;
+        onJoin(userInfo, roomId, socket);
+        console.log('join', instanceRoomId, Object.keys(attendees).length);
+      });
+
+      // 룸퇴장
+      socket.on('leave', (roomId, userInfo) => {
+        instanceRoomId = null;
+        onLeave(userInfo, roomId, socket);
+        console.log('leave', roomId, Object.keys(attendees).length);
+      });
+
+      // 메시징 (userMessage, typing, webrtc signaling, etc)
+      // Ojbect의 key값으로 메시지 type을 구분하여 중복형태의 메시지 전송 리스너 생성을 방지한다.
+      socket.on('message', (data) => {
+        console.log('message', data, instanceRoomId);
+
+        if (!instanceRoomId) {
+          return false;
         }
-      }
-    });
 
-    // 소켓 연결해제
-    socket.on('disconnect', () => {
-      console.log('a user disconnected', socket.id);
+        onMessage(data, instanceRoomId, socket);
+      });
+
+      // 소켓 연결해제
+      socket.on('disconnect', () => {
+        console.log('a user disconnected', socket.id, instanceRoomId);
+
+        for (let key in attendees) {
+          if (attendees[key].socketId === socket.id) {
+            const userInfo = attendees[key];
+            onLeave(userInfo, instanceRoomId, socket);
+
+            break;
+          }
+        }
+      });
     });
-  });
+  }
+
+  function initialize() {
+    bindSocketConnectionEvent();
+  }
+  initialize();
 };
