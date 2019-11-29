7fd1514c660f82b67d2fa257aa74f4dca2f968f1
dodortus <dodortus@gmail.com>
2019-11-29 17:59:35 +0900

    Update github corner

--- frontend/src/components/layout/ThemeChanger/ThemeChanger.js
+++ frontend/src/components/layout/ThemeChanger/ThemeChanger.js
@@ -5,6 +5,7 @@ import './ThemeChanger.scss';
 const ThemeChanger = () => {
   const changeTheme = (theme) => {
     document.querySelector('#codejs').setAttribute('data-theme', theme);
+    document.querySelector('.github-corner').setAttribute('data-theme', theme);
     utils.setStorage('theme', theme);
   };
 
