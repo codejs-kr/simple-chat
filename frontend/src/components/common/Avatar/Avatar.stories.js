c757856ac7984ce2acebc2d1414a5b2e8c80deaa
dodortus <dodortus@gmail.com>
2020-01-27 12:12:51 +0900

    Update storybook

---
+++ frontend/src/components/common/Avatar/Avatar.stories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Avatar from './Avatar';
+
+export default {
+  title: 'components|common/Avatar',
+  component: Avatar,
+};
+
+export const Default = () => (
+  <Avatar
+    src="https://cdn0.iconfinder.com/data/icons/profession-vol-1/32/avatar_superman_DC_comics_superhero_character_inspiression_-128.png"
+    alt="아바타"
+  />
+);
