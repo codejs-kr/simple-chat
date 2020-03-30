9a5b7df7136237542a367f46d53b1198658b8780
dodortus <dodortus@gmail.com>
2020-03-30 23:23:36 +0900

    Add react-window

--- frontend/src/components/room/ChatMessages/ChatMessages.js
+++ frontend/src/components/room/ChatMessages/ChatMessages.js
@@ -1,24 +1,9 @@
 import React from 'react';
+import { FixedSizeList as List } from 'react-window';
+import AutoSizer from 'react-virtualized-auto-sizer';
 import Avatar from 'components/common/Avatar';
 import moment from 'moment';
 import './ChatMessages.scss';
-
-const ChatMessages = ({ myInfo, messages }) => {
-  return (
-    <ul id="messages">
-      {messages.map((data, i) => {
-        // console.log(data, i);
-
-        if (data.type === 'chat') {
-          const isMine = myInfo.id === data.id;
-          return <UserMessage key={i} isMine={isMine} data={data} />;
-        }
-
-        return <SystemMessage key={i} data={data} />;
-      })}
-    </ul>
-  );
-};
 
 const UserMessage = ({ isMine, data }) => {
   const className = `chat ${isMine ? 'mine' : 'attendee'}`;
@@ -56,5 +41,38 @@ const SystemMessage = ({ data }) => {
     </li>
   );
 };
+
+const Row = ({ index, style }) => (
+  <div className={index % 2 ? 'ListItemOdd' : 'ListItemEven'} style={style}>
+    Row {index}
+  </div>
+);
+
+// const ChatMessages = ({ myInfo, messages }) => {
+//   return (
+//     <ul id="messages">
+//       {messages.map((data, i) => {
+//         // console.log(data, i);
+
+//         if (data.type === 'chat') {
+//           const isMine = myInfo.id === data.id;
+//           return <UserMessage key={i} isMine={isMine} data={data} />;
+//         }
+
+//         return <SystemMessage key={i} data={data} />;
+//       })}
+//     </ul>
+//   );
+// };
+
+const ChatMessages = ({ myInfo, messages }) => (
+  <AutoSizer>
+    {({ height, width }) => (
+      <List className="List" height={height} itemCount={1000} itemSize={60} width={width}>
+        {Row}
+      </List>
+    )}
+  </AutoSizer>
+);
 
 export default ChatMessages;
